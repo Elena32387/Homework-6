e2eda34fb7e91b17ee0d1e20847f755a0fc29ac1
Elena32387 <iea20ster@gmail.com>
2024-12-03 14:46:23 +0300

    ДЗ строки часть 1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        //Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName+ " " + firstName + " " +middleName;
+        System.out.println("Ф.И.О. сотрудника - "+ fullName);
+
+        //Задача 2
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - "+fullName.toUpperCase());
+
+        //Задача 3
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullName2True = fullName2.replace("ё" , "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName2True);
 
 
     }
